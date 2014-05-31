67e36fe14f80819d1e2d8d8c9c7193368a26fe0f
Isaac Mailach <isaac.mailach@gmail.com>
2014-05-31 13:25:14 -0400

    Make About aside sticky on Home page

---
+++ js/home.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > $('.content').offset().top) {
+            $('.content-aside').css({'position': 'fixed', 'right': '30px', 'top': '30px'});
+        } else if ($(document).scrollTop() < $('.content').offset().top) {
+            $('.content-aside').css({'position': 'static'});
+        }
+    });
+});
