9b78692edb6bfe4c83bde95a7d4e3688c5af899e
Isaac Mailach <isaac.mailach@gmail.com>
2014-06-15 21:33:55 -0400

    Add item info popup v1

--- js/global.js
+++ js/global.js
@@ -3,3 +3,6 @@ $(document).ready(function () {
         $('.footer').css({'position': 'absolute', 'bottom': '0'});
     }
 });
+var ItemInfoPopup = function(item_id) {
+        alert('I have been clicked!' + item_id);
+}
