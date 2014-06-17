9526769ed19c2e8fa39e27410f1e49d24fe17409
Isaac Mailach <isaac.mailach@gmail.com>
2014-06-17 16:27:43 -0400

    Add item info popup v2

--- js/global.js
+++ js/global.js
@@ -2,7 +2,13 @@ $(document).ready(function () {
     if ($(document).height() > $(window).height() === false) {
         $('.footer').css({'position': 'absolute', 'bottom': '0'});
     }
+    $('.content-block-item-heading').click(function () {
+        $.get('data/item_info.xml', function (item_info){
+                alert('success');
+        })
+            .fail(function() {
+                alert('fail');
+            });
+        $(this).parent().data('id')
+    });
 });
-var ItemInfoPopup = function(item_id) {
-        alert('I have been clicked!' + item_id);
-}
