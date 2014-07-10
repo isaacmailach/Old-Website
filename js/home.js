55fb5bbb86c4892fa0f63f92aa6df5ea0213b595
Isaac Mailach <isaac.mailach@gmail.com>
2014-07-10 13:58:05 -0400

    Change UI
    Roughly implemented flexbox. Added shadows and hover animation to items.

--- js/home.js
+++ js/home.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 
     // Fixed About Block
     $(window).scroll(function() {
-        if ($(document).scrollTop() > $('.content').offset().top) {
-            $('.content-aside').css({'position': 'fixed', 'right': '30px', 'top': '30px'});
-        } else if ($(document).scrollTop() < $('.content').offset().top) {
+        if ($(document).width() > 1020 && $(document).scrollTop() > $('.content').offset().top) {
+            $('.content-aside').css({'position': 'fixed', 'right': '30px', 'top': '30px'});    
+        } else {
             $('.content-aside').css({'position': 'static'});
         }
     });
