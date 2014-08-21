5e9a6cb5c8f18657b7c2a1ea4149d566096de7c8
Isaac Mailach <isaac.mailach@gmail.com>
2014-08-20 20:51:56 -0400

    Add animations to info popup
    Sticky nav bar with drop shadow added. Optimize popup layout for mobile.
    Add exit button and hide info button. Fix item switching in popup. Add
    drop shadow to item in view.

--- js/home.js
+++ js/home.js
@@ -1,27 +1,22 @@
 $(document).ready(function() {
 
     // Fixed About Block
-    $(window).scroll(function() {
-        if ($(document).scrollTop() > $('.content').offset().top && viewport().width > 1147) {
-            $('.content-aside').css({'position': 'fixed', 'right': '15px', 'top': '15px'});
-            if (viewport().width > 1192) {
-                $('.content-aside').css({'right': '30px', 'top': '30px'});
-            }
-        } else {
-            $('.content-aside').css({'position': 'static'});
-        }
+    $(window).scroll(function () {
+        CheckScrollPosition2();
     });
-    $(window).resize(function() {
-        if ($(document).scrollTop() > $('.content').offset().top && viewport().width > 1147) {
-            $('.content-aside').css({'position': 'fixed', 'right': '15px', 'top': '15px'});
-            if (viewport().width > 1192) {
-                $('.content-aside').css({'right': '30px', 'top': '30px'});
-            }
-        } else {
-            $('.content-aside').css({'position': 'static'});
-        }
+    $(window).resize(function () {
+        CheckScrollPosition2();
     });
-    
-    // "Read more" Link
     
 });
+
+function CheckScrollPosition2 () {
+    if ($(document).scrollTop() > $('.content').offset().top - 50 && Viewport().width > 1047) {
+        $('.content-aside').css({'position': 'fixed', 'right': '15px', 'top': '65px'});
+        if (Viewport().width > 1092) {
+            $('.content-aside').css({'right': '30px', 'top': '80px'});
+        }
+    } else {
+        $('.content-aside').css({'position': 'static'});
+    }
+}
